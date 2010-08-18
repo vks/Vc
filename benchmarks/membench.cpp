7151e6e6bcac76556d97c735c9819711dcd9cb99
Matthias Kretz <kretz@kde.org>
2010-08-18 22:39:49 +0200

    --only switch to run a single test; add prefetches

--- benchmarks/membench.cpp
+++ benchmarks/membench.cpp
@@ -74,16 +74,50 @@ void testAddOne(WorkItem work)
     work.timer.Stop();
 }
 
+void testAddOnePrefetch(WorkItem work)
+{
+    const double_v one = 1.;
+
+    double *__restrict__ m = work.m.entries() + work.offset * double_v::Size;
+
+    work.timer.Start();
+    for (size_t i = 0; i < work.size; i += 4) {
+        Vc::prefetchForModify(m + 1024);
+        (double_v(m + 0) + one).store(m + 0);
+        (double_v(m + 2) + one).store(m + 2);
+        (double_v(m + 4) + one).store(m + 4);
+        (double_v(m + 6) + one).store(m + 6);
+        m += 8;
+    }
+    work.timer.Stop();
+}
+
 void testRead(WorkItem work)
 {
     double *__restrict__ m = work.m.entries() + work.offset * double_v::Size;
     work.timer.Start();
     for (size_t i = 0; i < work.size; i += 4) {
-        const __m128d v0 = _mm_load_pd(m + 0);
-        const __m128d v1 = _mm_load_pd(m + 2);
-        const __m128d v2 = _mm_load_pd(m + 4);
-        const __m128d v3 = _mm_load_pd(m + 6);
-        asm("" :: "x"(v0), "x"(v1), "x"(v2), "x"(v3));
+        const double_v v0(m + 0);
+        const double_v v1(m + 2);
+        const double_v v2(m + 4);
+        const double_v v3(m + 6);
+        asm("" :: "x"(v0.data()), "x"(v1.data()), "x"(v2.data()), "x"(v3.data()));
+        m += 8;
+    }
+    work.timer.Stop();
+}
+
+void testReadPrefetch(WorkItem work)
+{
+    double *__restrict__ m = work.m.entries() + work.offset * double_v::Size;
+    work.timer.Start();
+    for (size_t i = 0; i < work.size; i += 4) {
+        Vc::prefetchForOneRead(m + 1024);
+        const double_v v0(m + 0);
+        const double_v v1(m + 2);
+        const double_v v2(m + 4);
+        const double_v v3(m + 6);
+        asm("" :: "x"(v0.data()), "x"(v1.data()), "x"(v2.data()), "x"(v3.data()));
         m += 8;
     }
     work.timer.Stop();
@@ -120,6 +154,10 @@ static T valueForArgument(const char *name, T defaultValue)
 
 static void executeTest(const char *name, MemT &mem, void (*testFun)(WorkItem))
 {
+    static std::string only = valueForArgument("--only", std::string());
+    if (!only.empty() && only != name) {
+        return;
+    }
     for (size_t offset = 0; offset <= mem.vectorsCount() - step; offset += step) {
         std::stringstream ss;
         ss << name << ": " << offset * sizeof(double_v) / GB << " - "
@@ -165,7 +203,9 @@ int bmain()
         sched_setaffinity(0, sizeof(cpu_set_t), &cpumask);
         executeTest("bzero", mem, &testBzero);
         executeTest("read",  mem, &testRead);
+        executeTest("read w/ prefetch", mem, &testReadPrefetch);
         executeTest("add 1", mem, &testAddOne);
+        executeTest("add 1 w/ prefetch", mem, &testAddOnePrefetch);
         Benchmark::finalize();
     }
     return 0;
